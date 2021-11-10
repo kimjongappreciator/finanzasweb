5cb7756f8a29bbc0766bd9a09732a9dbb3f76e10
kimjongappreciator <nonexistentexistentialist1@gmail.com>
2021-11-10 11:58:46 -0500

    db

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,5 +1,8 @@
 <template>
-<p>123123123</p>
+  <v-container style="background-color: darkorchid">
+  <p>123123123</p>
+  <router-link to="/home"> <v-btn> Iniciar sesion</v-btn></router-link>
+  </v-container>
 </template>
 
 <script>
